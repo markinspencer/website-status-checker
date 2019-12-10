96be3199cf8b8d0fcfaf0ca8b8bbaf5519abc9c5
Spencer Markin <smarkin@questica.com>
2019-12-10 07:35:19 -0500

    pause 5 seconds inbetween requests

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,8 +21,11 @@ func main() {
 		go checkURL(url, c)
 	}
 
-	for {
-		go checkURL(<-c, c)
+	for u := range c {
+		go func(url string) {
+			time.Sleep(5 * time.Second)
+			checkURL(url, c)
+		}(u)
 	}
 }
 
